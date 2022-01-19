c98191c41e656889cb3726d54eacaf322f92bd1c
uma-kamuni <kamuniumaramesh@gmail.com>
2022-01-18 23:49:07 -0800

    Added: Searching and viewing people by region using java streams

--- src/main/java/com/bridgelabz/addressbooksystem/AddressBookDirectoryIF.java
+++ src/main/java/com/bridgelabz/addressbooksystem/AddressBookDirectoryIF.java
@@ -4,12 +4,20 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public interface AddressBookDirectoryIF {
+	
 	public void addAddressBook();
+
 	public void operationDirectory();
+
 	public void displayDirectoryContents();
+
 	public void editAddressBook();
+
 	public void searchByCity();
+
 	public void searchByState();
+
 	public void displayPeopleByRegion(HashMap<String, ArrayList<ContactPerson>> listToDisplay);
+
 	public void countPeopleByRegion(HashMap<String, ArrayList<ContactPerson>> listToDisplay);
 }
