c3d3ba969091476a7a029123177d9b496f1ab4fe
uma-kamuni <kamuniumaramesh@gmail.com>
2022-01-18 23:52:41 -0800

    Refactored: formatted toString method

--- src/main/java/com/bridgelabz/addressbooksystem/Address.java
+++ src/main/java/com/bridgelabz/addressbooksystem/Address.java
@@ -32,6 +32,6 @@ public class Address {
 	@Override
 	public String toString() {
 		
-		return "\nCity - "+city+"\nState - "+state+"\nZip Code - "+zip;
+		return ", City - "+city+", State - "+state+", Zip Code - "+zip;
 	}
 }
