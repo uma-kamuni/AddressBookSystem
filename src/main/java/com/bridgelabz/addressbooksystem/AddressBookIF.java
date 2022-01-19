46b610b3a42ef8e03ba1b6ac98f270528e1de75b
uma-kamuni <kamuniumaramesh@gmail.com>
2022-01-18 23:46:28 -0800

    Refactored: Method to write and read from file

--- src/main/java/com/bridgelabz/addressbooksystem/AddressBookIF.java
+++ src/main/java/com/bridgelabz/addressbooksystem/AddressBookIF.java
@@ -1,10 +1,34 @@
 package com.bridgelabz.addressbooksystem;
 
+import java.util.List;
+import java.util.Scanner;
+
+import com.bridgelabz.addressbooksystem.AddressBook.IOService;
+
+
 public interface AddressBookIF {
-	
+
 	public void operation();
-	public void addContact();
+	
+	public void createContactPerson(Scanner scannerObject);
+
+	public void addContact(String firstName, ContactPerson person);
+
 	public void displayContents();
+
 	public void editPerson();
+
 	public void deletePerson();
+
+	public void addPersonToCity(ContactPerson contact);
+
+	public void addPersonToState(ContactPerson contact);
+
+	public void printSortedList(List<ContactPerson> sortedContactList);
+
+	public void sortAddressBook(int sortingChoice);
+	
+	public void writeToAddressBookFile(IOService ioService);
+
+	public List<String> readDataFromFile(IOService fileIo);
 }
