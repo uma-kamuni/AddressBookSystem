fd6fb8cadbf8f44ada361f5958ef79876db7e8f8
uma-kamuni <kamuniumaramesh@gmail.com>
2022-01-18 23:19:13 -0800

    Added: Delete person from addressbook

---
+++ src/main/java/com/bridgelabz/addressbooksystem/AddressBookIF.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.addressbooksystem;
+
+public interface AddressBookIF {
+	
+	public void operation();
+	public void addContact();
+	public void displayContents();
+	public void editPerson();
+	public void deletePerson();
+}
